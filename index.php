cd061f785e7fcc732f7ee78cdab6e437de8ffa38
mahsan <mahsan@vmrocky8.example.com>
2024-01-30 14:05:07 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+	<head> 
+	<title>PHP Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h>PHP Test</h1>
+		<p><b>A New Donderdag Example of PHP in Action</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	</body> 
+</html>
